bd1e449a95cb596793c17ac3a4b1f54723234359
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-12 12:18:04 -0500

    feat: :sparkles: Solution exercise ten
    The exercise ten is about calculate the sum, average, amount of numbers entered, bigger number, smaller number  of a string of numbers.

--- js/main.js
+++ js/main.js
@@ -1,37 +1,29 @@
-/* EJERCICIO 9
-N atletas han pasado a finales en salto triple en los juegos
-olímpicos femenino de 2022. Diseñe un programa que pida por
-teclado los nombres de cada atleta finalista y a su vez, sus
-marcas del salto en metros. Informar el nombre de la atleta
-campeona que se quede con la medalla de oro y si rompió
-récord, reportar el pago que será de 500 millones. El récord
-esta en 15,50 metros.
+/* EJERCICIO 10
+Desarrolle un programa cíclico que capture un dato
+numérico cada vez, y los vaya acumulando. El programa se
+detiene cuando el usuario digita un cero. El programa debe
+mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL
+PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR
+VALOR Y MENOR VALOR.
 */
-let finalistas = 0;
-let datos = [];
-let ganador = 0;
-let ganadorNombre = " ";
+let numeros = [];
+let numero;
+let suma = 0;
+let v = 0;
 
-finalistas = Number(prompt("Bienvenidos a las finales en salto triple \n Ingrese el numero de finalistas"))
-
-for (let i=0; i < finalistas; i++){
-    let nombre = prompt(`Ingresa el nombre del finalista ${i+1}: `);
-    let marca = Number(prompt(`Ingresa la marca del finalista ${i+1}: `));
-    datos.push({
-        Nombre: nombre,
-        Marca: marca,
-    })
-
-    if (marca > ganador){
-        ganador = marca;
-        ganadorNombre = nombre;
+do{
+    numero = Number(prompt(`Ingrese un numero`));
+    
+    if(numero>0){
+        numeros.push(numero);
+        suma = numero + suma;
     }
-}
 
-console.table(datos);
-console.log(`El ganador de la medalla de oro es: ${ganadorNombre}, con una marca: ${ganador}`)
-if (ganador > 15.50){
-    console.log("El ganador rampio el record, se realizara el pago de 500 millones")
-} else {
-    console.log("El ganador no rompio el record.")
-}
+}while (numero != 0);
+
+console.log(numeros);
+console.log(`Sumatoria de los valores: ${suma}`);
+console.log(`El promedio es: ${suma/numeros.length}`);
+console.log(`Cantidad de numeros digitados: ${numeros.length}`);
+console.log(`El valor mayor: ${Math.max(...numeros)}`);
+console.log(`El valor menor: ${Math.min(...numeros)}`);
