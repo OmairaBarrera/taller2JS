78e89750572634823633f30f3de482c896457571
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-12 10:32:41 -0500

    feat: :sparkles: Solution exercise seven
    The exersice seven is about calculate invoice from a client.

--- js/main.js
+++ js/main.js
@@ -1,60 +1,31 @@
-/* EJERCICIO 6
-Construir el algoritmo en Javascript para un programa
-para cualquier cantidad de estudiantes que lea el nombre,
-el sexo y la nota definitiva y halle al estudiante con la mayor
-nota y al estudiante con la menor nota y cuantos eran
-hombres y cuantos mujeres. 
+/* EJERCICIO 7
+Programa que pida el ingreso del nombre y precio de un artículo y la
+cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
+en su factura.
 */
-let datos = [];
+let factura = 0;
 let opcion = 0;
 let var_control = 0;
 let nombre = "";
-let sexo = "";
-let nota = 0;
-let nota_mayor = 0;
-let nota_menor = 100;
-let cont_m = 0;
-let cont_f = 0;
-
-//Inicio del flujo
-console.log("¡Bienvenido al sistema de registro de notas¡")
+let cantidad = 0;
+let precio = 0;
 
 do {
-    opcion  = Number(prompt("Menu Inicio \n\t1. Ingresar nuevo estudiante. \n\t2. Salir."));
+    opcion  = Number(prompt("Menu Inicio \n\t1. Ingresar nuevo producto. \n\t2. Salir."));
     if(opcion == 1){
-
-        nombre  = prompt("Ingrese el nombre del estudiante: ");
-        sexo    = prompt("Ingrese el sexo del estudiante (M: masculino -- F: femenino): ");
-        nota    = Number(prompt("Ingrese la nota (0% - 100%): "));
-
-        if(nota > nota_mayor){
-            nota_mayor      = nota;
-            nombre_mayor    = nombre;
-        }
-        if(nota < nota_menor ){
-            nota_menor      = nota;
-            nombre_menor    = nombre
-        }
-        if(sexo == "F"){
-            cont_f++;
-        }
-        if(sexo == "M"){
-            cont_m++;
-        }
+        nombre  = prompt("Ingrese el nombre del producto: ");
+        precio   = Number(prompt("Ingrese valor del producto: "));
+        cantidad = Number(prompt("Ingrese catidad: "));
+        
+        factura = (precio * cantidad) + factura;
     }
     if (opcion == 2){
         var_control = 1;
     }
     if (opcion != 1 && opcion != 2){
-        console.log("")
         console.log("Opcion no valida, vuelva a intentarlo...")
-        console.log("");
     }
 
 }while( var_control != 1);
 
-console.log("");
-console.log("Estudiante con mayor nota: ", nombre_mayor, "con un porcentaje: ", nota_mayor);
-console.log("Estudiante con menor nota: ", nombre_menor, "con un porcentaje: ", nota_menor);
-console.log("Numero de hombres:", cont_m);
-console.log("Numero de mujeres: ", cont_f);
+console.log("El total de su factura es: ", factura);
